a1982d16b6d53b6a5bc09ee1aed56a39777ef62b
Vlad Chuprin <chuprin.v@medindex.ru>
2023-08-13 19:57:13 +0300

    add timer closable instance

---
+++ lib/autoclosable/dart/closable_timer.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:autoclose/autoclosable/autoclosable.dart';
+import 'package:autoclose/closer/has_closer.dart';
+
+class ClosableTimer extends AutoClosable<Timer> {
+  ClosableTimer(super.closable, super.onClose);
+
+  @override
+  void close() {
+    return closable.cancel();
+  }
+
+  @override
+  bool? get isClosed => !closable.isActive;
+}
+
+extension SubscriptionClose on Timer {
+  void closeWith(HasCloser hasCloser, {void Function()? doOnClose}) {
+    hasCloser.closer.addClosable(ClosableTimer(this, doOnClose));
+  }
+}
