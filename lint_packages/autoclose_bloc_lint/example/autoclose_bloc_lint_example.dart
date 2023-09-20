b427f4816e4e5d40955c603ebf04d390ac2bb243
chuprin.v <chuprin.v@medindex.ru>
2023-09-20 12:20:01 +0300

    [re]moves

--- lint_packages/autoclose_bloc_lint/example/autoclose_bloc_lint_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:autoclose_bloc_lint/autoclose_bloc_lint.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lint_packages/autoclose_flutter_lint/example/autoclose_flutter_lint_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:autoclose_flutter_lint/autoclose_flutter_lint.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
