5764e442dbd5752fe426ffe802d965e7981452e0
chuprin.v <chuprin.v@medindex.ru>
2023-09-21 19:36:12 +0300

    preferredAssignment

--- packages/autoclose_lint/lib/src/closable/closable_lint_config.dart
+++ packages/autoclose_lint/lib/src/closable/closable_lint_config.dart
@@ -6,11 +6,20 @@ class ClosableLintConfig {
   final String closableTargetUrl;
   final String closableSourceUrl;
 
-  ClosableLintConfig(
-      {required this.name,
-      required this.userFriendlyName,
-      required this.closableTargetUrl,
-      required this.closableSourceUrl});
+  /// if our closable entity is designed only to close something (like `StreamSubscription`),
+  /// then we set `preferedAssignment = false`, which generates code fixes without assignment as we already handle closing
+  /// 
+  /// Opposite case `TextEditingController` which probably should be assigned to user's variable 
+  /// and also closed by autoclose lib
+  final bool preferredAssignment;
+
+  ClosableLintConfig({
+    required this.name,
+    required this.userFriendlyName,
+    required this.closableTargetUrl,
+    required this.closableSourceUrl,
+    this.preferredAssignment = true,
+  });
 
   late final Uri closableSourceLib = Uri.parse(closableSourceUrl);
   late final TypeChecker closableTypeChecker =
