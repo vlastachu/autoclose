567b1232ecbf89d4452d018b617eff5bc31fe97c
Vlad Chuprin <chuprin.v@medindex.ru>
2023-10-07 16:56:43 +0300

    setup termination for gc tests

--- gc_test/autoclose_test.dart
+++ gc_test/autoclose_test.dart
@@ -1,9 +1,20 @@
+import 'dart:io';
+
+import 'package:test/test.dart';
+
 import 'autoclosable/closable_future.dart';
 import 'autoclosable/closable_subscription.dart';
 import 'autoclosable/closable_timer.dart';
 
 void main() async {
+  tearDownAll(exitAfterTest);
   testClosableSubscription();
   testClosableTimer();
   testClosableFuture();
+}
+
+void exitAfterTest() {
+  // I'm not sure it's true but on exception I'll see that github runner failed with reaching timeout
+  print('All tests passed!');
+  exit(0);
 }
