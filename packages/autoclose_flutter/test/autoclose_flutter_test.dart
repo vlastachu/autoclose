a46d6ee827cf4fe267c9ca70a0e56d112980a48a
Vlad Chuprin <chuprin.v@medindex.ru>
2023-10-07 16:13:02 +0300

    run gc tests

--- packages/autoclose_flutter/test/autoclose_flutter_test.dart
+++ packages/autoclose_flutter/test/autoclose_flutter_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:autoclose_flutter/autoclose_flutter.dart';
-
 void main() {
   test('adds one to input values', () {
     // final calculator = Calculator();
