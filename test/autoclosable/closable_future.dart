26a679011ff6897902802de4f7e600a2d511c8fd
chuprin.v <chuprin.v@medindex.ru>
2023-10-15 15:05:31 +0300

    add some tests to describe onClose behavior

--- test/autoclosable/closable_future.dart
+++ test/autoclosable/closable_future.dart
@@ -18,50 +18,81 @@ void testClosableFuture() {
     test('closer close normally if future already done', () async {
       final closer = TestCloser();
       final mocked = MockSomeClass();
-      future() async {
-        await Future.delayed(Duration(milliseconds: 1)).closeWith(closer);
+      Future<void> future() async {
+        await Future.delayed(const Duration(milliseconds: 1)).closeWith(closer);
         mocked.someMethod();
       }
 
-      future();
+      unawaited(future());
 
       await untilCalled(mocked.someMethod());
       verify(mocked.someMethod()).called(1);
-      expect(() {
-        closer.close();
-      }, returnsNormally);
+      expect(closer.close, returnsNormally);
     });
     test('code after closed future doesnt executes', () async {
       final closer = TestCloser();
       final mocked = MockSomeClass();
-      future() async {
-        await Future.delayed(Duration(milliseconds: 1)).closeWith(closer);
+      Future<void> future() async {
+        await Future.delayed(const Duration(milliseconds: 1)).closeWith(closer);
         mocked.someMethod();
       }
 
       future();
       closer.close();
-      // TODO bad design of test
-      await Future.delayed(Duration(milliseconds: 100));
+      await Future.delayed(const Duration(milliseconds: 100));
       verifyNever(mocked.someMethod());
     });
-    // TODO well it is possibly a bad behavior
+    // BAD well it is possibly a bad behavior
     // probably need to add isClosed flag to closer and immidiately close all new closables.
     // On the other hand, this case is not expected: The library is designed in such a way that
     // closer creates it's own closables inside of own lifecycle
     test('calling close before future execution doesn\'t affects', () async {
       final closer = TestCloser();
       final mocked = MockSomeClass();
-      future() async {
-        await Future.delayed(Duration(milliseconds: 1)).closeWith(closer);
+      Future<void> future() async {
+        await Future.delayed(const Duration(milliseconds: 1)).closeWith(closer);
         mocked.someMethod();
       }
 
       closer.close();
-      future();
-      // TODO bad design of test
-      await Future.delayed(Duration(milliseconds: 100));
+      await future();
       verify(mocked.someMethod()).called(1);
+    });
+
+    test('doOnClose will not be called if future completes normally', () async {
+      final closer = TestCloser();
+      bool completed = false;
+      bool doOnCloseCalled = false;
+      Future<void> future() async {
+        await Future.delayed(const Duration(milliseconds: 1)).closeWith(closer, doOnClose: () {
+          doOnCloseCalled = true;
+        },);
+        completed = true;
+      }
+      await future();
+      expect(completed, isTrue);
+      expect(doOnCloseCalled, isFalse);
+      expect(closer.close, returnsNormally);
+      expect(completed, isTrue);
+      expect(doOnCloseCalled, isFalse);
+    });
+    test('doOnClose will be called if future is cancelled by closer', () async {
+      final closer = TestCloser();
+      bool completed = false;
+      bool doOnCloseCalled = false;
+      Future<void> future() async {
+        await Future.delayed(const Duration(milliseconds: 1)).closeWith(closer, doOnClose: () {
+          doOnCloseCalled = true;
+        },);
+        completed = true;
+      }
+      unawaited(future());
+      expect(closer.close, returnsNormally);
+      expect(completed, isFalse);
+      expect(doOnCloseCalled, isFalse); // close is async function
+      await Future.delayed(const Duration(milliseconds: 100));
+      expect(completed, isFalse);
+      expect(doOnCloseCalled, isTrue);
     });
   });
 }
