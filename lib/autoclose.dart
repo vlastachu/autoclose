f62b2383d30d2eacab84c144135af4513b75455d
chuprin.v <chuprin.v@medindex.ru>
2023-10-24 01:21:59 +0300

    import timer

--- lib/autoclose.dart
+++ lib/autoclose.dart
@@ -3,5 +3,6 @@ export 'closer/general_closer.dart';
 export 'closer/has_closer.dart';
 export 'single_autoclosable/dart/closable_future.dart';
 export 'single_autoclosable/dart/closable_subscription.dart';
+export 'single_autoclosable/dart/closable_timer.dart';
 export 'single_autoclosable/dart/do_on_close.dart';
 export 'single_autoclosable/single_autoclosable.dart';
